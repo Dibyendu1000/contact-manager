6ffb6ab10bc7c939e6598e3d5bb4d6adb3d31e97
Dibyendu Das <dibyendudgp10@gmail.com>
2024-10-16 17:33:47 +0530

    Init: NavBar created

---
+++ README.md
@@ -0,0 +1,4 @@
+# Contact-Manager
+
+- Basic React App to Add, Update, Delete and Search Contacts
+- Uses JSON-Server for backend operations
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="m-4 flex h-[60px] items-center justify-center rounded-lg bg-white text-xl font-medium">
+      <h1>Contact Manager</h1>
+    </div>
+  );
+};
+
+export default NavBar;
